0863df5e0acc3e23177e8eab6c1ca3065ec96f41
Brody Smith <bsmit272@uwo.ca>
2023-02-06 15:01:21 -0500

    Done

--- Lab1Part2.c
+++ Lab1Part2.c
@@ -114,19 +114,19 @@ int read_switches(void) {
 
 volatile int DELAY_LENGTH = 700000;
 int main(void) {
-    display_hex(-345434);
-    // int num;
-    // volatile int delay_count = 0;
-    // while(1) {
-    //     // on 
-    //     num = read_switches();
-    //     display_hex(num);
+    //display_hex(-345434);
+    int num;
+    volatile int delay_count = 0;
+    while(1) {
+        // on 
+        num = read_switches();
+        display_hex(num);
 
-    //     for (delay_count = DELAY_LENGTH; delay_count != 0; --delay_count) {}
+        for (delay_count = DELAY_LENGTH; delay_count != 0; --delay_count) {}
         
-    //     // off
-    //     display_hex(1000000);
-    //     for (delay_count = DELAY_LENGTH; delay_count != 0; --delay_count) {}
-    // }
+        // off
+        display_hex(1000000);
+        for (delay_count = DELAY_LENGTH; delay_count != 0; --delay_count) {}
+    }
 }
 
